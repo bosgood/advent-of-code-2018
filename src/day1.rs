b9de5a656373acb2381705cd9940a48180e46fc4
Brad Osgood <bosgood@gmail.com>
2018-12-01 13:24:48 -0500

    Refactor to use generator

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,5 @@
 #[aoc(day1, part1, Chars)]
-pub fn part1(input: &str) -> i32 {
+pub fn part1_chars(input: &str) -> i32 {
   input.lines().fold(0, |memo, l| {
     let int_formatted = l.trim_start_matches('+');
     let val = int_formatted.parse::<i32>().unwrap();
@@ -7,17 +7,38 @@ pub fn part1(input: &str) -> i32 {
   })
 }
 
+#[aoc_generator(day1)]
+pub fn number_generator(input: &str) -> Vec<i32> {
+  input
+    .lines()
+    .map(|l| {
+      let int_formatted = l.trim_start_matches('+');
+      int_formatted.parse::<i32>().unwrap()
+    }).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1_i32(input: &[i32]) -> i32 {
+  input.iter().fold(0, |memo, n| memo + n)
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
-  fn add2() {
-    assert_eq!(part1("-1\n+1"), 0);
+  fn part1_add2() {
+    let input = "-1\n+1";
+    let output = 0;
+    assert_eq!(part1_chars(input), output);
+    assert_eq!(part1_i32(&number_generator(input)), output);
   }
 
   #[test]
-  fn add4() {
-    assert_eq!(part1("-1\n+1\n+123123123\n-77039"), 123046084);
+  fn part1_add4() {
+    let input = "-1\n+1\n+123123123\n-77039";
+    let output = 123046084;
+    assert_eq!(part1_chars(input), output);
+    assert_eq!(part1_i32(&number_generator(input)), output);
   }
 }
