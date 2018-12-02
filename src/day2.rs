c237215c58838c460c150c5abfaa570c9b1dd3b2
Brad Osgood <bosgood@gmail.com>
2018-12-02 01:01:05 -0500

    Use reference type for strings here

--- src/day2.rs
+++ src/day2.rs
@@ -5,7 +5,7 @@ pub fn code_generator(input: &str) -> Vec<String> {
   input.lines().map(|l| String::from(l)).collect()
 }
 
-pub fn part1_count(input: &String) -> (i32, i32) {
+pub fn part1_count(input: &str) -> (i32, i32) {
   let mut found: HashMap<char, i32> = HashMap::new();
 
   for c in input.chars().into_iter() {
@@ -32,7 +32,7 @@ pub fn part1_count(input: &String) -> (i32, i32) {
 #[aoc(day2, part1)]
 pub fn part1_checksum(input: &[String]) -> i32 {
   let (doubles, triples) = input.iter().fold((0, 0), |memo, line| {
-    let (d, t) = part1_count(line);
+    let (d, t) = part1_count(&line);
     (memo.0 + d, memo.1 + t)
   });
   doubles * triples
@@ -58,49 +58,49 @@ mod tests {
   fn part1_count_example1() {
     let input = "abcdef";
     let output = (0, 0);
-    assert_eq!(part1_count(&String::from(input)), output);
+    assert_eq!(part1_count(&input), output);
   }
 
   #[test]
   fn part1_count_example2() {
     let input = "bababc";
     let output = (1, 1);
-    assert_eq!(part1_count(&String::from(input)), output);
+    assert_eq!(part1_count(&input), output);
   }
 
   #[test]
   fn part1_count_example3() {
     let input = "abbcde";
     let output = (1, 0);
-    assert_eq!(part1_count(&String::from(input)), output);
+    assert_eq!(part1_count(&input), output);
   }
 
   #[test]
   fn part1_count_example4() {
     let input = "abcccd";
     let output = (0, 1);
-    assert_eq!(part1_count(&String::from(input)), output);
+    assert_eq!(part1_count(&input), output);
   }
 
   #[test]
   fn part1_count_example5() {
     let input = "aabcdd";
     let output = (1, 0);
-    assert_eq!(part1_count(&String::from(input)), output);
+    assert_eq!(part1_count(&input), output);
   }
 
   #[test]
   fn part1_count_example6() {
     let input = "abcdee";
     let output = (1, 0);
-    assert_eq!(part1_count(&String::from(input)), output);
+    assert_eq!(part1_count(&input), output);
   }
 
   #[test]
   fn part1_count_example7() {
     let input = "ababab";
     let output = (0, 1);
-    assert_eq!(part1_count(&String::from(input)), output);
+    assert_eq!(part1_count(&input), output);
   }
 
   #[test]
