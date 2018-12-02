7f0e10c437fe54ef1477efc450f1d3afb3854eb0
Brad Osgood <bosgood@gmail.com>
2018-12-02 00:45:18 -0500

    day 2 part 1 completed

--- src/day2.rs
+++ src/day2.rs
@@ -1,18 +1,41 @@
-use im::HashSet as ImHashSet;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 #[aoc_generator(day2)]
 pub fn code_generator(input: &str) -> Vec<String> {
   input.lines().map(|l| String::from(l)).collect()
 }
 
-pub fn part1_count(input: &str) -> (i32, i32) {
-  (0, 0)
+pub fn part1_count(input: &String) -> (i32, i32) {
+  let mut found: HashMap<char, i32> = HashMap::new();
+
+  for c in input.chars().into_iter() {
+    let count = match found.get(&c) {
+      Some(c) => *c,
+      None => 0,
+    };
+    let _ = found.insert(c, count + 1);
+  }
+
+  let mut doubles = 0;
+  let mut triples = 0;
+  for v in found.values() {
+    match v {
+      2 => doubles += 1,
+      3 => triples += 1,
+      _ => {}
+    }
+  }
+
+  (doubles.min(1), triples.min(1))
 }
 
 #[aoc(day2, part1)]
 pub fn part1_checksum(input: &[String]) -> i32 {
-  0
+  let (doubles, triples) = input.iter().fold((0, 0), |memo, line| {
+    let (d, t) = part1_count(line);
+    (memo.0 + d, memo.1 + t)
+  });
+  doubles * triples
 }
 
 #[cfg(test)]
@@ -35,53 +58,53 @@ mod tests {
   fn part1_count_example1() {
     let input = "abcdef";
     let output = (0, 0);
-    assert_eq!(part1_count(input), output);
+    assert_eq!(part1_count(&String::from(input)), output);
   }
 
   #[test]
   fn part1_count_example2() {
     let input = "bababc";
     let output = (1, 1);
-    assert_eq!(part1_count(input), output);
+    assert_eq!(part1_count(&String::from(input)), output);
   }
 
   #[test]
   fn part1_count_example3() {
     let input = "abbcde";
     let output = (1, 0);
-    assert_eq!(part1_count(input), output);
+    assert_eq!(part1_count(&String::from(input)), output);
   }
 
   #[test]
   fn part1_count_example4() {
     let input = "abcccd";
     let output = (0, 1);
-    assert_eq!(part1_count(input), output);
+    assert_eq!(part1_count(&String::from(input)), output);
   }
 
   #[test]
   fn part1_count_example5() {
     let input = "aabcdd";
     let output = (1, 0);
-    assert_eq!(part1_count(input), output);
+    assert_eq!(part1_count(&String::from(input)), output);
   }
 
   #[test]
   fn part1_count_example6() {
     let input = "abcdee";
     let output = (1, 0);
-    assert_eq!(part1_count(input), output);
+    assert_eq!(part1_count(&String::from(input)), output);
   }
 
   #[test]
   fn part1_count_example7() {
     let input = "ababab";
     let output = (0, 1);
-    assert_eq!(part1_count(input), output);
+    assert_eq!(part1_count(&String::from(input)), output);
   }
 
   #[test]
-  fn part1_example() {
+  fn part1_integration() {
     let input = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab";
     let output = 12;
     assert_eq!(part1_checksum(&code_generator(&input)), output);
