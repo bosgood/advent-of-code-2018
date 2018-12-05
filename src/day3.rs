234833aa82fdd562c71f62a412acfd9b55aff276
Brad Osgood <bosgood@gmail.com>
2018-12-04 22:51:17 -0500

    Area function and unit tests

--- src/day3.rs
+++ src/day3.rs
@@ -10,6 +10,7 @@ use regex::Regex;
 */
 // FabricSquare expresses the elf's square of fabric in terms of its cartesian coordinates.
 // The origin point (0,0) is the top left point.
+#[derive(PartialEq, Eq, Debug)]
 pub struct FabricSquare {
   id: i32,
   x: i32,
@@ -28,6 +29,7 @@ impl FabricSquare {
           .unwrap();
     }
     let cap = re.captures(claim).unwrap();
+
     FabricSquare {
       id: 0,
       x: 0,
@@ -35,6 +37,11 @@ impl FabricSquare {
       width: 0,
       height: 0,
     }
+  }
+
+  // Calculates the area of this square
+  pub fn area(&self) -> i32 {
+    self.width * self.height
   }
 
   // Determines if this square overlaps with the given square
@@ -84,6 +91,101 @@ mod tests {
 
     The four square inches marked with X are claimed by both 1 and 2. (Claim 3, while adjacent to the others, does not overlap either of them.)
   */
+
+  #[test]
+  fn claim_to_fabric_square_test1() {
+    let input = "#1349 @ 724,871: 21x26";
+    let output = FabricSquare {
+      id: 1349,
+      x: 724,
+      y: 871,
+      width: 21,
+      height: 26,
+    };
+    assert_eq!(FabricSquare::new(&input), output);
+  }
+
+  #[test]
+  fn claim_to_fabric_square_test2() {
+    let input = "#23 @ 771,152: 16x19";
+    let output = FabricSquare {
+      id: 23,
+      x: 771,
+      y: 152,
+      width: 16,
+      height: 19,
+    };
+    assert_eq!(FabricSquare::new(&input), output);
+  }
+
+  #[test]
+  fn claim_to_fabric_square_test3() {
+    let input = "#2 @ 102,14: 23x14";
+    let output = FabricSquare {
+      id: 2,
+      x: 102,
+      y: 14,
+      width: 23,
+      height: 14,
+    };
+    assert_eq!(FabricSquare::new(&input), output);
+  }
+
+  #[test]
+  fn intersection_not_exists_test1() {
+    let s1 = FabricSquare {
+      id: 0,
+      x: 1,
+      y: 3,
+      width: 4,
+      height: 4,
+    };
+    let s3 = FabricSquare {
+      id: 2,
+      x: 5,
+      y: 6,
+      width: 2,
+      height: 2,
+    };
+    assert_eq!(s1.intersection(&s3).is_none(), true);
+  }
+
+  #[test]
+  fn intersection_exists_test1() {
+    let s1 = FabricSquare {
+      id: 0,
+      x: 1,
+      y: 3,
+      width: 4,
+      height: 4,
+    };
+    let s2 = FabricSquare {
+      id: 1,
+      x: 3,
+      y: 1,
+      width: 4,
+      height: 2,
+    };
+    let s0 = s1.intersection(&s2).unwrap();
+    assert_eq!(s0.x, 3);
+    assert_eq!(s0.y, 3);
+    assert_eq!(s0.height, 2);
+    assert_eq!(s0.width, 2);
+  }
+
+  #[test]
+  fn area_test1() {
+    assert_eq!(
+      FabricSquare {
+        id: 0,
+        x: 0,
+        y: 0,
+        width: 2,
+        height: 2,
+      }.area(),
+      4
+    );
+  }
 
   #[test]
   fn part1_overlap_integration_test() {
