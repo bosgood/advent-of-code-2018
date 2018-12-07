e3102d6f47906b9e0450ab6e3de670e893c05cfa
Brad Osgood <bosgood@gmail.com>
2018-12-07 00:16:33 -0500

    Scaffolding for day 4

---
+++ src/day4.rs
@@ -0,0 +1,11 @@
+pub struct Event {}
+
+#[aoc_generator(day4)]
+pub fn event_generator(input: &str) -> Vec<Event> {
+  vec![]
+}
+
+#[aoc(day4, part1)]
+pub fn day4_part1(input: &[Event]) -> i32 {
+  0
+}
