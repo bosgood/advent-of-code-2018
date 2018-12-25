ea343c056b964a79787d4573ee7c0a517567aa21
Brad Osgood <bosgood@gmail.com>
2018-12-24 22:29:44 -0500

    Day 5 scaffolding

---
+++ src/day5.rs
@@ -0,0 +1,17 @@
+// #[aoc_generator(day5)]
+// pub fn generator(input: &str) -> String {
+
+// }
+
+#[aoc(day5, part1)]
+pub fn day5_part1(input: &str) -> &'static str {
+    ""
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_event_from_str() {}
+}
